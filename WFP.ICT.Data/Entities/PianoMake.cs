e0362ff6cffc0496b895e753574f64b0ea78d107
qasimexalted <qasim.exalted@outlook.com>
2017-07-20 02:22:23 +0500

    Missing Files Added

---
+++ WFP.ICT.Data/Entities/PianoMake.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WFP.ICT.Data.Entities
+{
+    public class PianoMake : BaseEntity, iBaseEntity
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+        public Guid Id { get; set; }
+
+        public String Code { get; set; }
+
+        public string Name { get; set; }
+
+        public PianoMake()
+        {
+        }
+    }
+}
